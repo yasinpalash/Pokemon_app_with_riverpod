3ddf609ccad795e9c358cdfed423ef92e16a6238
yasinpalash <yasinmiapolash@gmail.com>
2025-04-29 16:42:25 +0600

    add provider to fetch Pokémon data from API

---
+++ lib/provides/pokemon_data_providers.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
+import 'package:testapp/models/pokemon.dart';
+import 'package:testapp/services/http_service.dart';
+
+final pokemonDataProvider = FutureProvider.family<Pokemon?, String>((
+  ref,
+  url,
+) async {
+  HTTPService _httpService = GetIt.instance.get<HTTPService>();
+  Response? response = await _httpService.get(url);
+
+  if (response != null && response.data != null) {
+    return Pokemon.fromJson(response.data);
+  }
+
+  return null;
+});
